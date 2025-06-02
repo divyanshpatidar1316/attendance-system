ec6f2ce123a45ae662cfe3c4d55e7e30d6632392
divyanshpatidar1316 <152731777+divyanshpatidar1316@users.noreply.github.com>
2025-06-02 22:53:37 +0530

    auth.js

--- client/js/auth.js
+++ client/js/auth.js
@@ -84,7 +84,7 @@ if (registerForm) {
         }
         
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('https://attendance-system-tlj2.onrender.com/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
